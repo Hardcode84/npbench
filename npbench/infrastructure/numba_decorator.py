8e15fddf1c376ed6d064ea74e28e723866c31ca8
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-05 16:57:14 +0200

    decorators

--- npbench/infrastructure/numba_decorator.py
+++ npbench/infrastructure/numba_decorator.py
@@ -1,13 +1,27 @@
 import numba as nb
+import os
 
-if False:
+_decorator = os.environ["NPBENCH_NUMBA_DECORATOR"]
+
+if _decorator == "numba":
     jit = nb.jit
-    njit = np.njit
+    njit = nb.njit
     vectorize = nb.vectorize
-else:
+elif _decorator == "numba-mlir":
     import numba_mlir
     jit = numba_mlir.jit
     njit = numba_mlir.njit
     vectorize = numba_mlir.vectorize
+elif _decorator == "replace-parfor":
+    import numba_mlir
+    def jit(*args, **kwargs):
+        return numba_mlir.jit(*args, *kwargs, replace_parfors=True)
+
+    def njit(*args, **kwargs):
+        return numba_mlir.njit(*args, *kwargs, replace_parfors=True)
+
+    vectorize = nb.vectorize
+else:
+    assert False, f"Invalid decorator {_decorator}"
 
 prange = nb.prange
